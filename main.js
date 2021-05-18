719be1f48edc28b05c5fe6390b64b7f7a813df9b
jtbc <jtbc@naver.com>
2021-05-18 22:22:08 +0900

    feat: 클릭된 버튼에 따른 title 변경 기능 추가
    클릭되는 카테고리 버튼에 따라 바로 아래 리스트들의 title이 변경되도록
    추가

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const weatherButtonsContainer = document.querySelector("#weatherContainer");
 
+// 날씨 카테고리 버튼 클릭 시 동작하는 함수
 function clickWeatherButtons(clickedButton) {
 	const buttons = clickedButton.parentNode.querySelectorAll("button");
+	const title = document.querySelector("#rank-weather");
+	let idx = 0;
+
+	// 버튼 click 상태에 따라 버튼 색상 변경
 	for (i = 0; i < buttons.length; i++) {
 		buttons[i].classList.remove("clicked");
 	}
-
 	clickedButton.classList.add("clicked");
+
+	// click된 버튼에 따라 title 변경
+	const weatherNames = [
+		"황사",
+		"폭염/열대야",
+		"이상고온",
+		"집중호우",
+		"태풍",
+		"폭설",
+		"한파",
+	];
+	for (i = 0; i < buttons.length; i++) {
+		if (buttons[i].classList.contains("clicked")) {
+			idx = i;
+		}
+	}
+	title.innerHTML = weatherNames[idx];
 }
 
-function init() {
-	// console.log(weatherButtonsContainer.querySelector(""));
-}
+function init() {}
 
 init();
